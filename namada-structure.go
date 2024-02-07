d821f777db21e0fc3a631d6ebef2912f4dcceefe
qj0r9j0vc2 <jinwoo794533@gmail.com>
2024-02-07 16:01:05 +0900

    add kubebuilder

--- namada-structure.go
+++ namada-structure.go
@@ -1,5 +1,6 @@
 package blockchain_toml
 
+// +kubebuilder:object:generate
 type NamadaConfigFile struct {
 	WasmDir *string       `json:"wasmDir" toml:"wasm_dir"`
 	Ledger  *NamadaLedger `json:"ledger" toml:"ledger"`
