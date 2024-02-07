7952361bdb225a794e0bb71d41901cba884b927a
qj0r9j0vc2 <jinwoo794533@gmail.com>
2024-02-07 14:21:48 +0900

    fix interface

--- interface.go
+++ interface.go
@@ -1,8 +1,8 @@
 package blockchain_toml
 
-type TomlFile *interface {
+type TomlFile interface {
 	MergeWithDefault() error
 	ExportMergeWithDefault() ([]byte, error)
-	MergeWithConfig(c *TomlFile) error
-	ExportMergeWithConfig(c *TomlFile) ([]byte, error)
+	MergeWithConfig(o *TomlFile) error
+	ExportMergeWithConfig(o *TomlFile) ([]byte, error)
 }
